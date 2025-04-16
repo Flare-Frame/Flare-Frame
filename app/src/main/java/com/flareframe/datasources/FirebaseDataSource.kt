67bd16d0748877c19ceffcadc463cfba01510d5c
Shravan Ramjathan <128147436+ST10247982@users.noreply.github.com>
2025-04-16 19:57:17 +0200

    Refactor FirebaseDataSource to public
    - Changed `FirebaseDataSource` interface and `FirebaseDataSourceImpl` class to be `public`.
    - Updated the selection state in `.idea/deploymentTargetSelector.xml` to `FlareFrame.app`.

--- app/src/main/java/com/flareframe/datasources/FirebaseDataSource.kt
+++ app/src/main/java/com/flareframe/datasources/FirebaseDataSource.kt
@@ -5,7 +5,7 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import javax.security.auth.callback.Callback
 
-interface FirebaseDataSource {
+public interface FirebaseDataSource {
     public fun createUserEmailAndPassword(email:String, password: String,callback:(Task<AuthResult>)-> Unit)
     public fun signInEmailAndPassword(email: String,password: String,calback:(Task<AuthResult>)-> Unit)
     public fun signOut()
--- app/src/main/java/com/flareframe/datasources/FirebaseDataSourceImpl.kt
+++ app/src/main/java/com/flareframe/datasources/FirebaseDataSourceImpl.kt
@@ -8,7 +8,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
 import javax.inject.Inject
 
-class FirebaseDataSourceImpl @Inject constructor(val auth: FirebaseAuth): FirebaseDataSource {
+public class FirebaseDataSourceImpl @Inject constructor(val auth: FirebaseAuth): FirebaseDataSource {
     override fun createUserEmailAndPassword(
         email: String,
         password: String,
