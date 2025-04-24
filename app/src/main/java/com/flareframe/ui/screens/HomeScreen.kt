86546f9d9bdb2f0326f3c1c0c1c47a93d32775f5
Shravan Ramjathan <128147436+ST10247982@users.noreply.github.com>
2025-04-24 09:38:23 +0200

    Refactor: Implement bottom navigation and update dependencies
    - Updated Android Gradle Plugin (AGP) to version 8.9.2 in `libs.versions.toml`.
    - Added `HomeScreen` composable.
    - Created a new `BottomNavigationItem` data class for managing navigation items.
    - Implemented `AppBottomBar` for bottom navigation in `GeneralComposables.kt` and added bottom nav items with icons.
    - Implemented `AppScaffold` to handle screen layout with snackbar and bottom nav.
    - Updated `FirebaseDataSourceImpl.kt` to include `callback` paramether in `signInEmailAndPassword`.
    - Refactored `LoginScreen` to navigate to `HomeScreen` and show a snackbar.
    - Added more color schemes in `Theme.kt`
    - Added icons dependency
    - Updated `MainActivity` to use `AppScaffold`.
    - Updated `LoginScreen` to use a navigation for the homescreen.
    - Updated `.idea` files for devices.
    - Updated `.idea` files for inspection.
    - Updated `build.gradle` to add navigation dependency.

---
+++ app/src/main/java/com/flareframe/ui/screens/HomeScreen.kt
@@ -0,0 +1,34 @@
+package com.flareframe.ui.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.flareframe.ui.states.UserUiState
+import com.flareframe.viewmodels.UserViewModel
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    userViewModel: UserViewModel,
+) {
+    val uiState: UserUiState by userViewModel.uiState.collectAsState()
+
+
+}
